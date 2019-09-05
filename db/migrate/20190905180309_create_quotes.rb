7bcc3d7b46c7dcb126b035b5398b087e0c45abfb
Tia Stutson <tmstutson@gmail.com>
2019-09-05 18:14:44 +0000

    updated the database for a saying and author. updated migrate

---
+++ db/migrate/20190905180309_create_quotes.rb
@@ -0,0 +1,12 @@
+class CreateQuotes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :quotes do |t|
+    	t.string :saying
+    	t.string :author
+
+      t.timestamps
+    end
+  end
+end
+
+
